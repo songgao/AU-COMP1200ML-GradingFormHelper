0b020b5fd490e7e845e0d569f71304227ea85aad
Song Gao <song.gao.beta@gmail.com>
2012-02-02 20:55:34 -0600

    1. will resize grading form window to 800*600 2. added zero-points button

--- content_script.js
+++ content_script.js
@@ -1,6 +1,6 @@
 $(function(){
-    $('body').append($('<div id="link_check_all">Full Points</div>'));
-    $('#link_check_all').click(function() {
+    $('body').append($('<div id="link_wrapper"><span id="link_zero_all" class="links">Zero Points</span><span id="link_check_all" class="links">Full Points</span></div>'));
+     $('#link_check_all').click(function() {
         var trs = $('tbody > tr');
         for(var tr_index = 0; tr_index < trs.length; ++tr_index){
             $(trs[tr_index]).find('td:nth-child(3) > input:first').click();
@@ -8,16 +8,33 @@ $(function(){
             setTimeout(function(){
                 trs.last().find('td:nth-child(3) > input:first').click();
             }, 500);
-    }).css({
+    });
+    $('#link_zero_all').click(function() {
+        var trs = $('tbody > tr');
+        for(var tr_index = 0; tr_index < trs.length; ++tr_index){
+            $(trs[tr_index]).find('td:nth-child(2) > input:first').click();
+        }
+            setTimeout(function(){
+                trs.last().find('td:nth-child(2) > input:first').click();
+            }, 500);
+    });
+    $('#link_wrapper').css({
         position: 'fixed',
         right: '12px',
-        top: '8px',
+        top: '8px'
+    });
+    $('.links').css({
+        float: 'right',
         color: 'red',
         "font-weight": 'bold',
         "text-decoration": "underline",
         "background-color": "black",
         cursor: 'pointer',
-        padding: '8px'
+        padding: '8px',
+        'margin-left': '4px',
+        'margin-right': '4px'
     });
+
+    window.resizeTo(800,600);
 
 });
